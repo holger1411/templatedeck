527437abcb6864173902f10468384462674a6e56
Holger Könemann <office@holgerkoenemann.de>
2022-06-02 13:33:26 +0200

    add fonts internally

--- .eleventy.js
+++ .eleventy.js
@@ -3,7 +3,7 @@ const navigationPlugin = require('@11ty/eleventy-navigation')
 const rssPlugin = require('@11ty/eleventy-plugin-rss')
 
 module.exports = function(eleventyConfig) {
-
+  eleventyConfig.addPassthroughCopy("src/fonts");
 
   function filterTagList(tags) {
     return (tags || []).filter(tag => ["all", "nav"].indexOf(tag) === -1);
